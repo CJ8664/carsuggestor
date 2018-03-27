521b9db4e59d131514dc70d55cb6af0d6eb362f0
Chirag Jain <csjain@ncsu.edu>
2018-03-27 14:29:32 -0400

    changes for other dataset

--- scripts/sentiment_analysis.py
+++ scripts/sentiment_analysis.py
@@ -28,16 +28,20 @@ def analyzeSentiment():
     """Get the review and perform sentiment analysis."""
     while True:
         carMake, carModel, reviewPath = getReview()
-
+        # print(carMake, carModel, reviewPath)
         if type(carMake) != Exception and carMake is not None:
-            carModel = '-'.join(carModel)
+            # carModel = '-'.join(carModel) keep for Chirag
+            if reviewPath == '' or reviewPath[-1] == '0':
+                continue
+            carModel = carModel[0]
+            carMake, carModel = carMake.lower(), carMake.lower()
             with open(reviewPath) as fileHandle:
                 reviewText = fileHandle.read()
                 reviewText = reviewText.replace('\'', '')
                 reviewBlob = TextBlob(reviewText, classifier=None)
                 reviewPolarity = reviewBlob.sentiment.polarity
                 reviewSubjectivity = reviewBlob.sentiment.subjectivity
-                insertQry = """INSERT INTO reviews (car_make, car_model,
+                insertQry = """INSERT INTO reviews2 (car_make, car_model,
                         review_polarity, review_subjectivity, review_text)
                         VALUES ('{}', '{}', {}, {}, '{}')""".format(
                     carMake, carModel, reviewPolarity, reviewSubjectivity,
@@ -59,7 +63,7 @@ def getReview():
             reviewPath = reviews.pop().split('/')
             carInfo = reviewPath[7].split('_')
             carMake = carInfo[0]
-            carModel = carInfo[1:-1]
+            carModel = carInfo[1]
             processedReviews += 1
             # print('Progress: {}'
             #       .format(round(processedReviews*100/reviewsCount, 2)))
@@ -80,7 +84,9 @@ def main():
     global reviews
     global reviewsCount
 
-    dataDir = '/Users/chiragjain/Documents/DDDM/project/data'
+    # dataDir = '/Users/chiragjain/Documents/DDDM/project/data1' Josh
+    # dataDir = '/Users/chiragjain/Documents/DDDM/project/data' Chirag
+    dataDir = '/Users/chiragjain/Documents/DDDM/project/data2'
 
     for dir in os.listdir(dataDir):
         if dir != '.DS_Store':
