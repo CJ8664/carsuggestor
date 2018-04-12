b85956f7a860b52d34ab067c059ab8807a2b01e8
Chirag Jain <csjain@ncsu.edu>
2018-04-12 14:15:23 -0400

    Script that has main logic

---
+++ public/js/main.js
@@ -0,0 +1,71 @@
+<script>
+  var q11output = document.getElementById("q11text");
+  var value;
+  q11output.innerHTML = "Pick Your First Choice";
+
+  function q11clickPic(value) {
+    if (value == 0) {
+      q11output.innerHTML = "Adventurous/Outdoorsy";
+    } else if (value == 1) {
+      q11output.innerHTML = "Unique";
+    } else if (value == 2) {
+      q11output.innerHTML = "Powerful";
+    } else if (value == 3) {
+      q11output.innerHTML = "Intellectual/Tech-Savvy";
+    } else if (value == 4) {
+      q11output.innerHTML = "Socialite";
+    } else if (value == 5) {
+      q11output.innerHTML = "Classy";
+    } else {
+      q11output.innerHTML = "Practical";
+    }
+    update();
+  }
+
+  var q12output = document.getElementById("q12text");
+  var value;
+  q12output.innerHTML = "Pick Your Second Choice";
+
+  function q12clickPic(value) {
+    if (value == 0) {
+      q12output.innerHTML = "Adventurous/Outdoorsy";
+    } else if (value == 1) {
+      q12output.innerHTML = "Unique";
+    } else if (value == 2) {
+      q12output.innerHTML = "Powerful";
+    } else if (value == 3) {
+      q12output.innerHTML = "Intellectual/Tech-Savvy";
+    } else if (value == 4) {
+      q12output.innerHTML = "Socialite";
+    } else if (value == 5) {
+      q12output.innerHTML = "Classy";
+    } else {
+      q12output.innerHTML = "Practical";
+    }
+    update();
+  }
+</script>
+<script>
+  var q13output = document.getElementById("q13text");
+  var value;
+  q13output.innerHTML = "Pick Your Third Choice";
+
+  function q13clickPic(value) {
+    if (value == 0) {
+      q13output.innerHTML = "Adventurous/Outdoorsy";
+    } else if (value == 1) {
+      q13output.innerHTML = "Unique";
+    } else if (value == 2) {
+      q13output.innerHTML = "Powerful";
+    } else if (value == 3) {
+      q13output.innerHTML = "Intellectual/Tech-Savvy";
+    } else if (value == 4) {
+      q13output.innerHTML = "Socialite";
+    } else if (value == 5) {
+      q13output.innerHTML = "Classy";
+    } else {
+      q13output.innerHTML = "Practical";
+    }
+    update();
+  }
+</script>
