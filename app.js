1e2487c1481be07675f6a986bc8fce2b33873bea
Chirag Jain <csjain@ncsu.edu>
2018-04-18 13:47:21 -0400

    Fixed app bug and added 500 page

--- app.js
+++ app.js
@@ -66,93 +66,97 @@ app.get('/', function(req, res) {
 // Get the result from Database
 app.post('/showcar', function(req, res) {
 
-      // Get a unique identifier for each form input data
-      var uuid = (new Date()).getTime();
-      req.session.uuid = uuid;
-      var fileName = "inquiry/" + uuid + ".json";
+  // Get a unique identifier for each form input data
+  var uuid = (new Date()).getTime();
+  req.session.uuid = uuid;
+  var fileName = "inquiry/" + uuid + ".json";
 
-      // Write a file containing the input from user
-      fs.writeFile(fileName, JSON.stringify(req.body), function(err) {
-        if (err) {
-          console.log(err);
-        } else {
-          // If data successfully written, call the python script to process the
-          // input
-          var process = spawn('python3', ['scripts/process.py', fileName]);
-          process.stdout.on('data', function(chunk) {
-            var execStatus = chunk.toString('utf8');
-            if (execStatus.startsWith('SUCCESS')) {
-              // Read the result that will be stored in _result file
-              fs.readFile("inquiry/" + uuid + "_result.json", function(err, cars) {
-                if (err) {
-                  console.log(err);
-                } else {
-                  // Get the reviews from the database for that car
-                  cars = JSON.parse(cars);
-                  console.log(cars);
+  // Write a file containing the input from user
+  fs.writeFile(fileName, JSON.stringify(req.body), function(err) {
+    if (err) {
+      console.log(err);
+      res.render('500');
+    } else {
+      // If data successfully written, call the python script to process the
+      // input
+      var process = spawn('python3', ['scripts/process.py', fileName]);
+      process.stdout.on('data', function(chunk) {
+        var execStatus = chunk.toString('utf8');
+        if (execStatus.startsWith('SUCCESS')) {
+          // Read the result that will be stored in _result file
+          fs.readFile("inquiry/" + uuid + "_result.json", function(err, cars) {
+            if (err) {
+              console.log(err);
+            } else {
+              // Get the reviews from the database for that car
+              cars = JSON.parse(cars);
 
-                  var final_result = [];
-                  function sleep(time) {
-                    return new Promise((resolve) => setTimeout(resolve, time));
-                  }
-                  cars.forEach(function(car_name) {
-                      var car = car_name.split('_')
-                      var carMake = car[0]
-                      var carModel = car[1]
-                      // data = 'audi'
-                      var positiveQry = "SELECT review_text from reviews where car_make = '" +
-                        carMake + "' and car_model = '" + carModel + "' order by review_polarity desc LIMIT 3"
-                      var negativeQry = "SELECT review_text from reviews where car_make = '" +
-                        carMake + "' and car_model = '" + carModel + "' order by review_polarity LIMIT 3"
-
-                      var posReviews = [];
-                      var negReviews = [];
-
-                      var posQryDone = false;
-
-                      client.query(positiveQry, (err, result) => {
-                        result.rows.forEach(function(value) {
-                          posReviews.push(value.review_text);
-                        });
-                      })
-                      client.query(negativeQry, (err, result) => {
-                        result.rows.forEach(function(value) {
-                          negReviews.push(value.review_text);
-                        });
-                      })
-                      sleep(3000).then(() => {
-                        final_result.push({
-                          'car': carMake.toUpperCase() + ' ' + carModel.toUpperCase(),
-                          'positive': posReviews,
-                          'negative': negReviews
-                        })
-                        // console.log(final_result);
-                        // Render the result
-                        res.render('result', {
-                          cars: final_result
-                        });
-                      });
-                    })
-                  }
-                });
+              var final_result = [];
+              function sleep(time) {
+                return new Promise((resolve) => setTimeout(resolve, time));
               }
-            });
-          }
-        });
-      })
+              cars.forEach(function(car_name) {
+                  var car = car_name.split('_')
+                  var carMake = car[0]
+                  var carModel = car[1]
+                  // data = 'audi'
+                  var positiveQry = "SELECT review_text from reviews where car_make = '" +
+                    carMake + "' and car_model = '" + carModel + "' order by review_polarity desc LIMIT 3"
+                  var negativeQry = "SELECT review_text from reviews where car_make = '" +
+                    carMake + "' and car_model = '" + carModel + "' order by review_polarity LIMIT 3"
 
-    // Save the result
-    app.post('/save', function(req, res) {
-      if (req.body.happy == 'yes') {
-        fs.rename("inquiry/" + req.session.uuid + ".json", "correct/" +
-          req.session.uuid + ".json");
-        fs.rename("inquiry/" + req.session.uuid + "_result.json", "correct/" +
-          req.session.uuid + "_result.json");
-      } else {
-        fs.rename("inquiry/" + req.session.uuid + ".json", "incorrect/" +
-          req.session.uuid + ".json");
-        fs.rename("inquiry/" + req.session.uuid + "_result.json", "incorrect/" +
-          req.session.uuid + "_result.json");
-      }
-      res.render('thankyou');
-    })
+                  var posReviews = [];
+                  var negReviews = [];
+
+                  var posQryDone = false;
+
+                  client.query(positiveQry, (err, result) => {
+                    result.rows.forEach(function(value) {
+                      posReviews.push(value.review_text);
+                    });
+                  })
+                  client.query(negativeQry, (err, result) => {
+                    result.rows.forEach(function(value) {
+                      negReviews.push(value.review_text);
+                    });
+                  })
+                  sleep(1500).then(() => {
+                    final_result.push({
+                      'car': carMake.toUpperCase() + ' ' + carModel.toUpperCase(),
+                      'positive': posReviews,
+                      'negative': negReviews
+                    })
+                  });
+              })
+              // console.log(final_result);
+              // Render the result
+              sleep(4500).then(() => {
+                res.render('result', {
+                  cars: final_result
+                });
+              });
+            }
+          });
+        } else {
+          res.render('500');
+        }
+      });
+    }
+  });
+})
+
+// Save the result
+app.post('/save', function(req, res) {
+  if (req.body.happy == 'yes') {
+    fs.rename("inquiry/" + req.session.uuid + ".json", "correct/" +
+      req.session.uuid + ".json");
+    fs.rename("inquiry/" + req.session.uuid + "_result.json", "correct/" +
+      req.session.uuid + "_result.json");
+  } else {
+    fs.rename("inquiry/" + req.session.uuid + ".json", "incorrect/" +
+      req.session.uuid + ".json");
+    fs.rename("inquiry/" + req.session.uuid + "_result.json", "incorrect/" +
+      req.session.uuid + "_result.json");
+  }
+  res.render('thankyou');
+})
