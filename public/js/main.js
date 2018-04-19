9a6c48ab2e9f4018f922d685db07c53398e73083
Chirag Jain <csjain@ncsu.edu>
2018-04-19 17:07:57 -0400

    Updated the survey

--- public/js/main.js
+++ public/js/main.js
@@ -4,14 +4,14 @@ var q12output = document.getElementById("q12text");
 q12output.value = "Pick Your Second Choice";
 var q13output = document.getElementById("q13text");
 q13output.value = "Pick Your Third Choice";
-var q14output = document.getElementById("q14text");
-q14output.value = "Pick Your Fourth Choice";
-var q15output = document.getElementById("q15text");
-q15output.value = "Pick Your Fifth Choice";
-var q16output = document.getElementById("q16text");
-q16output.value = "Pick Your Sixth Choice";
-var q17output = document.getElementById("q17text");
-q17output.value = "Pick Your Seventh Choice";
+// var q14output = document.getElementById("q14text");
+// q14output.value = "Pick Your Fourth Choice";
+// var q15output = document.getElementById("q15text");
+// q15output.value = "Pick Your Fifth Choice";
+// var q16output = document.getElementById("q16text");
+// q16output.value = "Pick Your Sixth Choice";
+// var q17output = document.getElementById("q17text");
+// q17output.value = "Pick Your Seventh Choice";
 var slider = document.getElementById("q2");
 var q2output = document.getElementById("q2text");
 q2output.value = "Pick An Image";
@@ -88,78 +88,78 @@ function q13clickPic(value) {
   }
 }
 
-function q14clickPic(value) {
-  if (value == 0) {
-    q14output.value = "Adventurous/Outdoorsy";
-  } else if (value == 1) {
-    q14output.value = "Unique";
-  } else if (value == 2) {
-    q14output.value = "Powerful";
-  } else if (value == 3) {
-    q14output.value = "Intellectual/Tech-Savvy";
-  } else if (value == 4) {
-    q14output.value = "Socialite";
-  } else if (value == 5) {
-    q14output.value = "Classy";
-  } else {
-    q14output.value = "Practical";
-  }
-}
-
-function q15clickPic(value) {
-  if (value == 0) {
-    q15output.value = "Adventurous/Outdoorsy";
-  } else if (value == 1) {
-    q15output.value = "Unique";
-  } else if (value == 2) {
-    q15output.value = "Powerful";
-  } else if (value == 3) {
-    q15output.value = "Intellectual/Tech-Savvy";
-  } else if (value == 4) {
-    q15output.value = "Socialite";
-  } else if (value == 5) {
-    q15output.value = "Classy";
-  } else {
-    q15output.value = "Practical";
-  }
-}
-
-function q16clickPic(value) {
-  if (value == 0) {
-    q16output.value = "Adventurous/Outdoorsy";
-  } else if (value == 1) {
-    q16output.value = "Unique";
-  } else if (value == 2) {
-    q16output.value = "Powerful";
-  } else if (value == 3) {
-    q16output.value = "Intellectual/Tech-Savvy";
-  } else if (value == 4) {
-    q16output.value = "Socialite";
-  } else if (value == 5) {
-    q16output.value = "Classy";
-  } else {
-    q16output.value = "Practical";
-  }
-}
-
-function q17clickPic(value) {
-  if (value == 0) {
-    q17output.value = "Adventurous/Outdoorsy";
-  } else if (value == 1) {
-    q17output.value = "Unique";
-  } else if (value == 2) {
-    q17output.value = "Powerful";
-  } else if (value == 3) {
-    q17output.value = "Intellectual/Tech-Savvy";
-  } else if (value == 4) {
-    q17output.value = "Socialite";
-  } else if (value == 5) {
-    q17output.value = "Classy";
-  } else {
-    q17output.value = "Practical";
-  }
-
-}
+// function q14clickPic(value) {
+//   if (value == 0) {
+//     q14output.value = "Adventurous/Outdoorsy";
+//   } else if (value == 1) {
+//     q14output.value = "Unique";
+//   } else if (value == 2) {
+//     q14output.value = "Powerful";
+//   } else if (value == 3) {
+//     q14output.value = "Intellectual/Tech-Savvy";
+//   } else if (value == 4) {
+//     q14output.value = "Socialite";
+//   } else if (value == 5) {
+//     q14output.value = "Classy";
+//   } else {
+//     q14output.value = "Practical";
+//   }
+// }
+//
+// function q15clickPic(value) {
+//   if (value == 0) {
+//     q15output.value = "Adventurous/Outdoorsy";
+//   } else if (value == 1) {
+//     q15output.value = "Unique";
+//   } else if (value == 2) {
+//     q15output.value = "Powerful";
+//   } else if (value == 3) {
+//     q15output.value = "Intellectual/Tech-Savvy";
+//   } else if (value == 4) {
+//     q15output.value = "Socialite";
+//   } else if (value == 5) {
+//     q15output.value = "Classy";
+//   } else {
+//     q15output.value = "Practical";
+//   }
+// }
+//
+// function q16clickPic(value) {
+//   if (value == 0) {
+//     q16output.value = "Adventurous/Outdoorsy";
+//   } else if (value == 1) {
+//     q16output.value = "Unique";
+//   } else if (value == 2) {
+//     q16output.value = "Powerful";
+//   } else if (value == 3) {
+//     q16output.value = "Intellectual/Tech-Savvy";
+//   } else if (value == 4) {
+//     q16output.value = "Socialite";
+//   } else if (value == 5) {
+//     q16output.value = "Classy";
+//   } else {
+//     q16output.value = "Practical";
+//   }
+// }
+//
+// function q17clickPic(value) {
+//   if (value == 0) {
+//     q17output.value = "Adventurous/Outdoorsy";
+//   } else if (value == 1) {
+//     q17output.value = "Unique";
+//   } else if (value == 2) {
+//     q17output.value = "Powerful";
+//   } else if (value == 3) {
+//     q17output.value = "Intellectual/Tech-Savvy";
+//   } else if (value == 4) {
+//     q17output.value = "Socialite";
+//   } else if (value == 5) {
+//     q17output.value = "Classy";
+//   } else {
+//     q17output.value = "Practical";
+//   }
+//
+// }
 
 function clickPic(num) {
   slider.value = num;
